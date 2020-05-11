adeea72ccde08986e74d51d81e1c883d0d7f5591
zhangjinlong <zhangjinlongll@163.com>
2020-05-11 19:55:01 +0800

    fix: 文件上传

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 var ws = require("nodejs-websocket");
-var Excute = require("./routes/excute.js");
+// var Excute = require("./routes/excute.js");
 console.log("开始建立连接...")
 
-var game1 = null, game1Ready = false;
 var server = ws.createServer(function(conn){
     conn.on("text", function (str) {
         console.log("收到的信息为:" + str)
         // 路由地址
         if(str === "/api/geneShellByConfig"){
-            game1 = conn;
-            game1Ready = true;
             conn.sendText("success");
         }
+        conn.sendText(str);
         // 执行脚本
-        Excute('hello-world', '', conn)
+        // Excute('hello-world', '', conn)
     })
     conn.on("close", function (code, reason) {
         console.log("关闭连接")
---
+++ remind.md
@@ -0,0 +1,7 @@
+## nodemon
+安装nodemon可以热加载node服务
+修改代码的时候，不需要再每次重启服务
+全局安装
+```
+npm i nodemon -g 
+```
